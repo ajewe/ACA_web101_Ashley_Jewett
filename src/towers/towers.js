afebc152d5d5fa5c93858fab3495917e2a79048d
ajewe <ajewe@utexas.edu>
2019-10-12 13:36:43 -0500

    created towers game, made things wider

---
+++ src/towers/towers.js
@@ -0,0 +1,20 @@
+
+  let $block = null;
+  
+  $('[data-row]').click(function() {
+
+    if ($block) {
+      $(this).append($block);
+      $block = null;
+
+    } else {
+      $block = $(this).children().last().detach();
+    }
+  })
+
+  $( function() {
+    $( "#draggable" ).draggable();
+
+
+
+
