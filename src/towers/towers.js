87e7753bc68cbf7b46b15a5761ff7bfc2dbe5e89
ajewe <ajewe@utexas.edu>
2019-10-13 22:50:58 -0500

    added some logic to how I would approach this problem bc this is way too advanced for me and i feel defeated and hate everything

--- src/towers/towers.js
+++ src/towers/towers.js
@@ -1,20 +1,28 @@
 
-  let $block = null;
+  let block = null;
   
+  function checkIfValidMove() {
+    //if blue block, can be placed on any row
+    //if green block, can be placed on row unless prev block is blue
+    //if red block, can be placed on row unless prev block is blue or green
+    //if yellow block, can be placed on row if no prev block
+  }
+
   $('[data-row]').click(function() {
 
-    if ($block) {
-      $(this).append($block);
-      $block = null;
+    if (block) {
+      console.log('dropping', block.attr('id'))
+      console.log('dropping on row', $(this).attr('class'))
+      console.log('row already has blocks', $(this).children().toArray())
+      for (let block of $(this).children().toArray()) {
+        console.log('row has block', block.id)
+      }
+      $(this).append(block);
+      block = null;
 
     } else {
-      $block = $(this).children().last().detach();
+      block = $(this).children().last().detach();
     }
-
-    function checkIfValidMove() {
-      //if 
-    }
-
   })
 
 
